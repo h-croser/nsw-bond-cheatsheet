584f76ea9ca5cd9834db0cb7bddefa709ea686e5
Hamish Croser <h.croser1@gmail.com>
2024-04-13 16:50:34 +1000

    Added sitemap creation in build action

---
+++ utils/build_sitemap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+URL="https://nswrentalbonds.info/"
+PAGES=("data" "about")
+PAGE_DATE="2024-04-13"
+FILE_PREFIX="_file/data/"
+FILES=("holdings.csv" "refunds-portions.csv" "refunds-totals.csv")
+CURR_DATE=$(date +"%Y-%m-%d")
+CHANGE_FREQ="monthly"
+
+# Create new sitemap
+exec 1> docs/sitemap.xml
+
+# Write head
+echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+# Write root URL
+echo "<url>"
+echo " <loc>${URL}${endpoint}</loc>"
+echo " <lastmod>${PAGE_DATE}</lastmod>"
+echo " <changefreq>${CHANGE_FREQ}</changefreq>"
+echo " <priority>1.0</priority>"
+echo "</url>"
+
+# Write page URLs
+for endpoint in "${PAGES[@]}"; do
+  echo "<url>"
+  echo " <loc>${URL}${endpoint}</loc>"
+  echo " <lastmod>$PAGE_DATE</lastmod>"
+  echo " <changefreq>$CHANGE_FREQ</changefreq>"
+  echo " <priority>0.8</priority>"
+  echo "</url>"
+done
+
+# Write file URLs
+for file in "${FILES[@]}"; do
+  echo "<url>"
+  echo " <loc>${URL}${FILE_PREFIX}${file}</loc>"
+  echo " <lastmod>${CURR_DATE}</lastmod>"
+  echo " <changefreq>${CHANGE_FREQ}</changefreq>"
+  echo " <priority>0.5</priority>"
+  echo "</url>"
+done
+
+# print foot
+echo "</urlset>"
